6e73755067348f29e170acd3753b7b36fce24cd4
Benjmain Zwahlen <benzwahlen@gmail.com>
2024-09-21 11:25:30 +0100

    Added mysql refresh connection function

--- src/common/storage/database/DB.php
+++ src/common/storage/database/DB.php
@@ -7,12 +7,21 @@ use benjaminzwahlen\bracemvc\common\exceptions\DatabaseConnectionException;
 class DB
 {
     public static $db = null;
+    private string $host;
+    private string $user;
+    private string $password;
+    private string $dbName;
 
-    public static function init($host, $user,  #[\SensitiveParameter] $password, $dbname)
+    public static function init($host, $user,  #[\SensitiveParameter] $password, $dbName)
     {
         try {
             // Create connection
-            DB::$db = new \mysqli($host, $user, $password, $dbname);
+            DB::$db = new \mysqli($host, $user, $password, $dbName);
+            DB::$host = $host;
+            DB::$user = $user;
+            DB::$password = $password;
+            DB::$dbName = $dbName;
+            
         } catch (\Exception $e) {
             throw new DatabaseConnectionException("Unable to connect to database: " . $e->getMessage());
         }
@@ -20,5 +29,10 @@ class DB
         if (DB::$db->connect_error) {
             throw new DatabaseConnectionException("Unable to connect to database: " . DB::$db->connect_error);
         }
+    }
+
+    public static function refreshConnection()
+    {
+        if( !\mysqli_ping(DB::$db) ) DB::$db = new \mysqli(DB::$host, DB::$user, DB::$password, DB::$dbName);
     }
 }
